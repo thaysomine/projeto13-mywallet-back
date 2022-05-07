d43a06a4ca13e93c74142373e26c22c03e263b00
Thays Omine <thaysomine@gmail.com>
2022-05-07 04:16:33 -0300

    feat: sign-in post added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import joi from 'joi';
+import bcrypt from 'bcrypt';
+import {v4} from 'uuid';
 
 const app = express();
 app.use(express.json()); 
@@ -32,8 +34,8 @@ app.post('/sign-up', async (req, res) => {
 
     const schema = joi.object({
         name: joi.string().required(),
-        email: joi.string().required(),
-        password: joi.string().required(),
+        email: joi.string().required().pattern(/\S+@\S+\.\S+/),
+        password: joi.string().required().pattern(/(?=^.{8,}$)((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/),
         confirmPassword: joi.string().required().valid(joi.ref('password'))
     });
     const validation = schema.validate(user);
@@ -45,19 +47,66 @@ app.post('/sign-up', async (req, res) => {
 
     try {
         const checkName = await database.collection('users').findOne({name: body.name});
+        const checkEmail = await database.collection('users').findOne({email: body.email});
         if (checkName) {
             res.status(409).send('User já cadastrado');
             console.log('User já cadastrado');
             return;
         }
-
-        await database.collection('users').insertOne(user);
-        console.log('user cadastrado', user);
+        if (checkEmail) {
+            res.status(409).send('Email já cadastrado');
+            console.log('Email já cadastrado');
+            return;
+        }
+        
+        const hash = await bcrypt.hash(body.password, 10);
+        await database.collection('users').insertOne({name: body.name, email: body.email, password: hash});
+        console.log('user cadastrado', {name: body.name, email: body.email, password: hash});
         res.sendStatus(201);
     } catch {
         res.status(422).send("Erro ao cadastrar");
     }
 });
+
+// post para logar um usuário
+app.post('/sign-in', async (req, res) => {
+    const body = req.body;
+    console.log('body do login', body);
+    const user = {
+        email: body.email,
+        password: body.password
+    }
+
+    const schema = joi.object({
+        email: joi.string().required().pattern(/\S+@\S+\.\S+/),
+        password: joi.string().required()
+    });
+    const validation = schema.validate(user);
+    if(validation.error){
+        console.log('Erro ao logar usuário', validation.error);
+        res.status(422).send('Erro ao logar usuario');
+        return;
+    }
+
+    try {
+        const userdb = await database.collection('users').findOne({email: body.email});
+        if (userdb && await bcrypt.compare(body.password, userdb.password)) {
+            const token = v4();
+            console.log('user logado', token);
+            await database.collection('sessions').insertOne({token, user: userdb._id});
+            res.send(userdb).status(200);
+        } else {
+            res.status(401).send('Usuario e/ou senha incorretos');
+            console.log('Usuario e/ou senha incorretos');
+        }
+    } catch {
+        res.status(401).send('Usuario e/ou senha incorretos');
+        console.log('Usuario e/ou senha incorretos');
+    }
+
+});
+
+
 
 app.listen(5000);
 
